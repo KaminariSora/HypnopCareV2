b1a7a8caaf2f78f5047705db48efa342ce7c9d1d
KaminariSora <nonthacha.h@gmail.com>
2025-02-14 17:02:21 +0700

    Function selected gender

--- hypno_care_v2/lib/page2_bmi.dart
+++ hypno_care_v2/lib/page2_bmi.dart
@@ -8,6 +8,15 @@ class Page2Bmi extends StatefulWidget {
 }
 
 class _Page2BmiState extends State<Page2Bmi> {
+  String status = 'Normal';
+  double bmi = 0.0;
+  String? selectedGender;
+
+  void selectGender(String gender) {
+    setState(() {
+      selectedGender = gender;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,36 +35,32 @@ class _Page2BmiState extends State<Page2Bmi> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              const SizedBox(height: 10,),
+              const SizedBox(
+                height: 10,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    width: 180,
-                    height: 166,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFF6EBD9),
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: const [BoxShadow(blurRadius: 5)],
-                    ),
-                    child: Image.asset('assets/plus_button.png'),
+                  GenderButton(
+                    gender: 'MALE',
+                    imagePath: 'assets/male_icon.png',
+                    isSelected: selectedGender == 'MALE',
+                    onSelect: selectGender,
                   ),
                   const SizedBox(
                     width: 10,
                   ),
-                  Container(
-                    width: 180,
-                    height: 166,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFF6EBD9),
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: const [BoxShadow(blurRadius: 5)],
-                    ),
-                    child: Image.asset('assets/plus_button.png'),
+                  GenderButton(
+                    gender: 'FEMALE',
+                    imagePath: 'assets/female_icon.png',
+                    isSelected: selectedGender == 'FEMALE',
+                    onSelect: selectGender,
                   ),
                 ],
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(
+                height: 10,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -71,7 +76,9 @@ class _Page2BmiState extends State<Page2Bmi> {
                   ),
                 ],
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(
+                height: 10,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -100,7 +107,9 @@ class _Page2BmiState extends State<Page2Bmi> {
                   ),
                 ],
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(
+                height: 10,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -108,20 +117,22 @@ class _Page2BmiState extends State<Page2Bmi> {
                     width: 365,
                     height: 57,
                     child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFF5E7F60)
-                      ),
-                      onPressed: (){}, 
-                      child: const Text(
-                        'Calculate',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFFFFFFFF)
-                        ),)),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF5E7F60)),
+                        onPressed: () {},
+                        child: const Text(
+                          'Calculate',
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFFFFFFFF)),
+                        )),
                   )
-              ],),
-              const SizedBox(height: 10,),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
               SizedBox(
                 width: 365,
                 child: Row(
@@ -131,24 +142,28 @@ class _Page2BmiState extends State<Page2Bmi> {
                     SizedBox(
                       height: 115,
                       child: Image.asset(
-                        'assets/h.png',),
+                        'assets/happy_face.png',
+                      ),
                     ),
-                    const SizedBox(width: 20,),
+                    const SizedBox(
+                      width: 20,
+                    ),
                     Column(
                       children: [
                         Text(
-                          'Normal',
-                          style: TextStyle(
-                            fontSize: 24,
-                            color: Color(0xFF36AE7C),
-                            fontWeight: FontWeight.bold
-                          ),),
+                          status,
+                          style: const TextStyle(
+                              fontSize: 24,
+                              color: Color(0xFF36AE7C),
+                              fontWeight: FontWeight.bold),
+                        ),
                         Text(
-                          '0.0',
-                          style: TextStyle(
+                          bmi.toString(),
+                          style: const TextStyle(
                             fontSize: 64,
                             color: Color(0xFF4F513C),
-                          ),),
+                          ),
+                        ),
                       ],
                     )
                   ],
@@ -156,6 +171,57 @@ class _Page2BmiState extends State<Page2Bmi> {
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class GenderButton extends StatelessWidget {
+  final String gender;
+  final String imagePath;
+  final bool isSelected;
+  final Function(String) onSelect; // Callback function
+
+  const GenderButton({
+    super.key,
+    required this.gender,
+    required this.imagePath,
+    required this.isSelected,
+    required this.onSelect,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onSelect(gender); // Notify parent about the selection
+      },
+      child: Container(
+        width: 180,
+        height: 166,
+        decoration: BoxDecoration(
+          color: isSelected ? const Color(0xFFBFBB95) : const Color(0xFFF6EBD9),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: const [BoxShadow(blurRadius: 5)],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Image.asset(
+                imagePath,
+                width: 100,
+                height: 100,
+                fit: BoxFit.contain,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              gender,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
