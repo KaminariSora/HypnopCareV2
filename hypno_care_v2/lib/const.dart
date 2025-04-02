99db76efface4132c6cae62069da56079e40be66
NaphatNu <132038675+NaphatNu@users.noreply.github.com>
2025-04-02 21:42:20 +0700

    edit ollama -> gemini

--- hypno_care_v2/lib/const.dart
+++ hypno_care_v2/lib/const.dart
@@ -1,3 +1,3 @@
-const String ollamaAPI = '/api/generate';
-const String ngrokUrl =
-    "https://4abb-2001-fb1-13-2cfc-7491-8e87-dae0-e54c.ngrok-free.app";
+const String geminiAPI = 'AIzaSyDEJwUwfYZ1zOgd0KtpAqv7bs9kd7DjJRg';
+const String apiUrl =
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
